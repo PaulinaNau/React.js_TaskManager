e2d0a2cfae5e10687255396e1154e04e2ed2801f
PaulinaNau <paulina.naurecka@gmail.com>
2023-08-22 11:32:01 +0200

    final commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./index.css";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./assets/style/theme";
-import Articles from "./components/organism/Articles/Articles";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NotFound from "./components/organism/PageNotFound/PageNotFound";
 import NewTaskManager from "./components/organism/NewTaskManager/NewTaskManager";
@@ -14,7 +13,6 @@ const App = () => {
       <ThemeProvider theme={theme}>
         <Routes>
           <Route exact path="/" element={<NewTaskManager />}></Route>
-          <Route path="/news" element={<Articles />}></Route>
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </ThemeProvider>
--- src/data/TaskData.js
+++ src/data/TaskData.js
@@ -2,25 +2,25 @@ export const TaskData = [
     {
         title: "Learn React.js",
         body: "Read about styled-components",
-        id: 12,
+        id: 0,
         done: false
     },
     {
         title: "Learn JS",
         body: "lorem",
-        id: 13,
+        id: 1,
         done: false
     },
     {
         title: "Go for shopping",
         body: "Lorem ipsum",
-        id: 14,
+        id: 2,
         done: false
     },
     {
         title: "Buy something",
         body: "Lorem ipsum",
-        id: 15,
+        id: 3,
         done: false
     }
 ]
